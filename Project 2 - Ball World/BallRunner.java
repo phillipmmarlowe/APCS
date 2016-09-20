6d091ed5ef613012c7b07045d8bc0a8b3d5ab3c6
pmarlowe791321 <791321@seq.org>
2016-09-20 08:35:33 -0700

    Add files via upload

--- Project 2 - Ball World/BallRunner.java
+++ Project 2 - Ball World/BallRunner.java
@@ -2,29 +2,32 @@
 /**
  * Write a description of class BallRunner here.
  * 
- * @author Phillip 
+ * @author Phillip Marlowe
  * @version (a version number or a date)
  */
 public class BallRunner
 {
     public static void run(){
-    BallWorld ballWorld = new BallWorld(500, 500);
-    TGPoint tGPoint = new TGPoint();
-    BallBot[] list = new BallBot[5];
-    for(int i = 0; i < 5; i++ ){
-    TGPoint tGPoint2 = new TGPoint(Math.random()*10, Math.random()*10 );
-    list[i] = new BallBot(ballWorld, tGPoint2, Math.random()*10, 10);
-    while (list[i].canMoveForward(ballWorld)){
-        list[i].moveForward();
-    }
-    }
-    }
-  
-    
-    
-    
-    
-    
+        BallWorld ballWorld = new BallWorld(500, 500);
+        TGPoint tGPoint = new TGPoint();
+        BallBot[] list = new BallBot[5];
+        for(int i = 0; i < 5; i++ ){
+            TGPoint tGPoint2 = new TGPoint(Math.random()*100, Math.random()*100 );
+            list[i] = new BallBot(ballWorld, tGPoint2, Math.random()*360, 25);
+            list[i].setPixelsPerSecond(100); 
+          
+        }
+        while (true){
+            for(int i = 0; i < 5; i++ ){
+            if (list[i].canMoveForward(ballWorld)){
+                    list[i].moveForward();
+                }else{
+                    list[i].setHeading(Math.random()*360);
+                    while (list[i].canMoveForward(ballWorld)){
+                        list[i].moveForward();
+                    }
+                }
+            }            
+            }
 }
-
-
+}
